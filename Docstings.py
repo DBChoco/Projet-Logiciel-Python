36bc18c5fc23e4caed4a99f194a5f09c467ac3c7
kbahmed <44785461+kbahmed@users.noreply.github.com>
2018-11-11 13:56:03 +0100

    Create Docstings.py

---
+++ Docstings.py
@@ -0,0 +1,20 @@
+"""
+Titre : Logiciel Python projet
+Date début : 5 novembre 2018
+Auteurs : Groupe n°33 projet
+"""
+"""
+Définition des variables utilisées
+"""
+#####################################################################
+"""
+Block Environnement
+"""
+#####################################################################
+"""
+Block Effet-de-Serre
+"""
+#####################################################################
+"""
+Block Ventillation
+""" 
